d36789056f8279b5860e3b689084bc81b03f7295
IKSAKS <55540304+IKSAKS@users.noreply.github.com>
2024-03-12 04:59:46 +0200

    main commit

---
+++ src/main/java/me/theabsolutepsychopath/modlink/ModLink.java
@@ -0,0 +1,17 @@
+package me.theabsolutepsychopath.modlink;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ModLink extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
